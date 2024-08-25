7d91e86ba60761b011f33730fa46c81d6b8b475a
medkhalilbk <medkhalilbenkhelil@gmail.com>
2024-08-25 22:49:40 +0100

    update for deliveryman / region

--- server.ts
+++ server.ts
@@ -1,59 +1,37 @@
 import express from 'express';
 import http from 'http';
 import { Server, Socket } from 'socket.io';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-
-// Type for the decoded JWT payload
-interface DecodedToken {
-  companyId: string;
-}
-
-// Middleware to handle authentication and extract companyId from the token
-io.use((socket: Socket, next: (err?: Error) => void) => {
-  const token = socket.handshake.query.token as string;
-
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.SECRET_KEY as string) as DecodedToken;
-      socket.data.companyId = decoded.companyId; // Store companyId in the socket object
-      next();
-    } catch (err) {
-      console.log('Authentication error:', (err as Error).message);
-      next(new Error('Authentication error'));
-    }
-  } else {
-    next(new Error('No token provided'));
-  }
-});
-
-// Handle new client connections
+const deliveryManData: { [region: string]: Array<{ id: string }> } = {};
 io.on('connection', (socket: Socket) => {
-  const companyId = socket.data.companyId as string;
+  console.log('A client connected');
+  socket.on('deliveryMan-join', (room) => {
+    const { id, region } = room;
+    console.log(`Delivery man with ID ${id} has joined the region ${region}`);
+    socket.join(region);
+    if (!deliveryManData[region]) {
+      deliveryManData[region] = [];
+    }  
+    Object.keys(deliveryManData).forEach((r) => {
+      let isDeliveryManExists = deliveryManData[r].find((deliveryMan) => deliveryMan.id === id);
+      if(isDeliveryManExists){
+        deliveryManData[r] = deliveryManData[r].filter((deliveryMan) => deliveryMan.id !== id);
+      }
+    })
+    deliveryManData[region].push({ id });
+    socket.emit('joined-room', { id, region });
+  });
 
-  if (companyId) {
-    socket.join(companyId);
-    console.log(`User ${socket.id} joined room: ${companyId}`);
 
-    socket.on('newOrder', (orderData: { companyId: string; [key: string]: any }) => {
-      io.to(companyId).emit('orderNotification', orderData);
-      console.log(`New order notification sent to room: ${companyId}`);
-    });
-
-    socket.on('disconnect', () => {
-      console.log('User disconnected:', socket.id);
-    });
-  } else {
-    console.log('No companyId available, connection refused.');
-    socket.disconnect();
-  }
+  socket.on('get-delivery-men', ({region}) => { 
+    const deliveryMen = deliveryManData[region] || [];
+    console.log(deliveryManData[region]) 
+    socket.emit('delivery-men-list', { region, deliveryMen });
 });
+}); 
 
 server.listen(8080, () => {
   console.log('WebSocket server is running on port 8080');
